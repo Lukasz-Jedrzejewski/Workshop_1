f4a19ad05fffd56afbecd6391647504936a47480
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-04-10 15:06:39 +0200

    Added conditions

--- src/pl/coderslab/Game_1/Main.java
+++ src/pl/coderslab/Game_1/Main.java
@@ -1,40 +1,54 @@
 package pl.coderslab.Game_1;
 
-import javax.sound.midi.Soundbank;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Now I will draw a number from 1 to 100...");
+        System.out.println();
         int rand = randNumber(100);
-        guessNumber(rand);
+            guessNumber(rand);
+
     }
-    static int randNumber(int max){
+
+    static int randNumber(int max) {
         Random randomNumber = new Random();
         int num = 0;
-        num = randomNumber.nextInt(max-1)+1;
-        System.out.println(num);
+        num = randomNumber.nextInt(max - 1) + 1;
 
         return num;
 
     }
-    static int guessNumber(int rand){
-        System.out.println("Gues number");
-        int guesNumber;
+
+    static int guessNumber(int rand) {
+        int guesNumber = 0;
+        int counter = 0;
         Scanner scan = new Scanner(System.in);
         while (true) {
+            System.out.println("Gues number");
+            while (!scan.hasNextInt()) {
+                scan.next();
+                System.out.println("This is not a number");
+            }
             guesNumber = scan.nextInt();
-            System.out.println(guesNumber);
+            if (guesNumber > 100){
+                System.out.println("Out of bound");
+                continue;
+            }
+            counter++;
+
             if (guesNumber > rand) {
                 System.out.println("Too big");
             } else if (guesNumber < rand) {
                 System.out.println("Too small");
             } else {
-                System.out.println("Got it");
+                System.out.println("Got it! " + "Your score is: " + counter);
                 break;
             }
         }
-            return guesNumber;
+
+        return guesNumber;
     }
 }
