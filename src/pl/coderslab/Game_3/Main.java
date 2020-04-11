30ac60c1c7210f90ea11e040ee1d19399bfc81a8
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-04-11 18:53:50 +0200

    Game ginished. Can be expanded

--- src/pl/coderslab/Game_3/Main.java
+++ src/pl/coderslab/Game_3/Main.java
@@ -9,29 +9,48 @@ public class Main {
     public static void main(String[] args) {
         guessNumber();
     }
-    static int guessNumber(){
-        System.out.println("Type your number in range of 1 - 1000." +
+
+    static int guessNumber() {
+        System.out.println("Type your number in range of 0 - 1000." +
                 "\nI will try to guess in max 10 attempts");
         int min = 0;
         int max = 1000;
+        int counter = 1;
         int guess = ((max - min) / 2) + min;
         String answer = "";
         Scanner scan = new Scanner(System.in);
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Answer: too much, not enough or you guessed it");
-            System.out.println("Guess: " + guess);
+            System.out.println("Guess " + counter + " " + i +" :" + guess);
             answer = scan.nextLine();
-            if ((!answer.equalsIgnoreCase("too much")) && (!answer.equalsIgnoreCase("not enough"))
-                    && (!answer.equalsIgnoreCase("you guesssed it")))
-                System.out.println("Bad answer");
-            if (answer.contains("too much")){
-                max = guess;
-                guess = ((max - min) / 2) + min;
-            } else if (answer.contains("not enough")){
-                min = guess;
-                guess = ((max - min) / 2) + min;
-            } else
+            counter++;
+//            if ((!answer.equalsIgnoreCase("too much")) && (!answer.equalsIgnoreCase("not enough"))
+//                    && (!answer.equalsIgnoreCase("you guessed it"))) {
+//            }
+            if (answer.equalsIgnoreCase("too much")) {
+                if (i == 9) {
+                    System.out.println("Don't cheat \nLet's try again\n");
+                    guessNumber();
+                } else {
+                    max = guess;
+                    guess = ((max - min) / 2) + min;
+                }
+            } else if (answer.equalsIgnoreCase("not enough")) {
+                if(i == 9) {
+                    System.out.println("Don't cheat \nLet's try again\n");
+                    guessNumber();
+                } else {
+                    min = guess;
+                    guess = ((max - min) / 2) + min;
+                }
+            } else if (answer.equalsIgnoreCase("you guessed it")) {
                 System.out.println("I won!");
+                break;
+            } else {
+                System.out.println("Bad answer");
+                i--;
+                counter--;
+            }
         }
         return guess;
     }
