10855037f7c1b551618a5771334715927027ab51
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-04-11 15:57:24 +0200

    Added draw and result. Code is pre-ready, can be expanded

--- src/pl/coderslab/Game_2/Main.java
+++ src/pl/coderslab/Game_2/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        yourNumbers();
+        result();
     }
 
     static List<Integer> yourNumbers() {
@@ -14,30 +14,53 @@ public class Main {
         int counter = 1;
         int typed = 0;
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter six numbers in range 1-49");
         while (counter < 7) {
             System.out.print("Enter number " + counter + ": ");
             while (!scan.hasNextInt()) {
                 scan.next();
-                System.out.print("This is not a number" + "\n" + "enter number " + counter + ": ");
+                System.out.print("This is not a number" + "\n" + "Enter number " + counter + ": ");
             }
             typed = scan.nextInt();
             if (typed < 1 || typed > 49) {
                 System.out.println("Number is out of range");
-            } else if(typedNumbers.contains(typed)) {
-                System.out.println("Number already exist");
+            } else if (typedNumbers.contains(typed)) {
+                System.out.println("This number has already been entered");
             } else {
                 typedNumbers.add(typed);
                 counter++;
             }
         }
         Collections.sort(typedNumbers);
-        System.out.println(typedNumbers);
+        System.out.println("Your six numbers: \n" + typedNumbers);
         return typedNumbers;
     }
 
+    static List<Integer> draw() {
+        Random numbers = new Random();
+        Set<Integer> dn = new HashSet<>();
+        while (dn.size() < 6) {
+            dn.add(numbers.nextInt(49) + 1);
+        }
+        List<Integer> drawNumbers = new ArrayList<Integer>(dn);
+        Collections.sort(drawNumbers);
+        System.out.println("Draw results: \n" + drawNumbers);
+        return drawNumbers;
+    }
 
+    static void result() {
+        List typed = yourNumbers();
+        List draw = draw();
+        draw.retainAll(typed);
+        int counter = draw.size();
+        if (counter >= 3)
+            System.out.println("You win!" + "\nYou hit " + counter + "!");
+        else
+            System.out.println("You won nothing \nYou hit " + counter);
+    }
 
 }
+
 
 
 
