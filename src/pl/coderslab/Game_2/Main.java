1887e708f587ba8902f3d9af1b8464f6ec1ef793
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-04-11 13:49:54 +0200

    new code with conditions

--- src/pl/coderslab/Game_2/Main.java
+++ src/pl/coderslab/Game_2/Main.java
@@ -1,30 +1,57 @@
 package pl.coderslab.Game_2;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         yourNumbers();
     }
-    static int[] yourNumbers(){
-        int[] typyingNumbers = new int[6];
-        System.out.println("Type six numbers from 1 to 49");
-        int yourTypyingNumber = 0;
+
+    static List<Integer> yourNumbers() {
+
+        List<Integer> typedNumbers = new ArrayList<Integer>(6);
+        int counter = 1;
+        int typed = 0;
         Scanner scan = new Scanner(System.in);
-            for (int i = 0; i < typyingNumbers.length; i++) {
-                System.out.print("Your number: ");
-//                while (!scan.hasNextInt()) {
-//                    scan.next();
-//                    System.out.println("This is not a number");
-//                }
-                yourTypyingNumber = scan.nextInt();
-                typyingNumbers[i] = yourTypyingNumber;
+        while (counter < 7) {
+            System.out.print("Enter number " + counter + ": ");
+            while (!scan.hasNextInt()) {
+                scan.next();
+                System.out.print("This is not a number" + "\n" + "enter number " + counter + ": ");
             }
-            Arrays.sort(typyingNumbers);
-            System.out.println(Arrays.toString(typyingNumbers));
-            return typyingNumbers;
+            typed = scan.nextInt();
+            if (typed < 1 || typed > 49) {
+                System.out.println("Number is out of range");
+            } else if(typedNumbers.contains(typed)) {
+                System.out.println("Number already exist");
+            } else {
+                typedNumbers.add(typed);
+                counter++;
+            }
+        }
+        Collections.sort(typedNumbers);
+        System.out.println(typedNumbers);
+        return typedNumbers;
     }
 
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
