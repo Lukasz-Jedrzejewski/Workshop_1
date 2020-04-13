e3787442f494ae5ade4d8f84153498065bf9f0b1
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-04-13 17:21:31 +0200

    Added Scanner method

--- src/pl/coderslab/Game_5/Main.java
+++ src/pl/coderslab/Game_5/Main.java
@@ -15,34 +15,11 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
-        // Wywołanie pobierania
-        Connection connect = Jsoup.connect("https://www.Onet.pl/");
-        List<String> titles = new ArrayList<>();
-        List<String> splited = new ArrayList<>();
-        try {
-            Document doc = connect.get();
-            Elements links = doc.select("span.title");
-            for (Element elem : links) {
-                titles.add(elem.text());
-            }
-            // Eliminacja zbędnych znaków/dzielenie na pojedyncze słowa
-            for (String line : titles) {
-                List<String> sl = Arrays.asList(line.split("\\p{Punct}+|\\s+|\\s+|\\d+"));
-                splited.addAll(sl);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        result(reading());
+    }
 
-        //usuwanie słów krótszych niż 3 znaki
-        List<String> withoutWordsShorterThanThreeCharacters = new ArrayList<>();
-        for (int i = 0; i < splited.size(); i++) {
-            if (splited.get(i).length() > 3)
-                withoutWordsShorterThanThreeCharacters.add(splited.get(i));
-        }
-
+    static void  result (List <String> withoutWordsShorterThanThreeCharacters) {
         //zapis do pliku
         Path path = Paths.get("/home/lukasz/Pulpit/javaCD/Workshop_1/src/pl/coderslab/Game_5/popular_words.txt");
         try {
@@ -91,4 +68,45 @@ public class Main {
         }
         System.out.println(reading.toString());
     }
+
+    static List<String> reading() {
+        Scanner scan = new Scanner(System.in);
+        String input;
+        Connection connect;
+        while (true) {
+            try {
+                System.out.println("Enter url");
+                input = scan.nextLine();
+                // Wywołanie pobierania
+                connect = Jsoup.connect(input);
+                break;
+            } catch (Exception e) {
+                System.out.println("Wrong url");
+            }
+        }
+        List<String> titles = new ArrayList<>();
+        List<String> splited = new ArrayList<>();
+        try {
+            Document doc = connect.get();
+            Elements links = doc.select("span.title");
+            for (Element elem : links) {
+                titles.add(elem.text());
+            }
+            // Eliminacja zbędnych znaków/dzielenie na pojedyncze słowa
+            for (String line : titles) {
+                List<String> sl = Arrays.asList(line.split("\\p{Punct}+|\\s+|\\s+|\\d+"));
+                splited.addAll(sl);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //usuwanie słów krótszych niż 3 znaki
+        List<String> withoutWordsShorterThanThreeCharacters = new ArrayList<>();
+        for (int i = 0; i < splited.size(); i++) {
+            if (splited.get(i).length() > 3)
+                withoutWordsShorterThanThreeCharacters.add(splited.get(i));
+        }
+        return withoutWordsShorterThanThreeCharacters;
+    }
+
 }
