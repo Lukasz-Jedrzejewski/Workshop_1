72f2555ab5a9213dc29970a90428e608bd0b3ecf
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-04-13 15:46:08 +0200

    Added filters

--- src/pl/coderslab/Game_5/Main.java
+++ src/pl/coderslab/Game_5/Main.java
@@ -14,36 +14,45 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.ConcurrentModificationException;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Connection connect = Jsoup.connect("https://www.onet.pl/");
+        Connection connect = Jsoup.connect("https://www.Onet.pl/");
         List<String> titles = new ArrayList<>();
+        List<String> splited = new ArrayList<>();
+
         try {
             Document doc = connect.get();
             Elements links = doc.select("span.title");
             for (Element elem : links) {
                 titles.add(elem.text());
+
+            }
+            for (String line : titles) {
+                List<String> sl = Arrays.asList(line.split("\\p{Punct}+|\\s+|\\s+|\\d+"));
+                splited.addAll(sl);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         System.out.println(titles);
+        System.out.println(splited);
+        List<String> withoutSmallthan3 = new ArrayList<>();
+        for (int i = 0; i < splited.size(); i++) {
+            if (splited.get(i).length() > 3)
+                withoutSmallthan3.add(splited.get(i));
+        }
+        System.out.println(withoutSmallthan3);
         Path path = Paths.get("/home/lukasz/Pulpit/javaCD/Workshop_1/src/pl/coderslab/Game_5/popularWords.txt");
         try {
             PrintWriter printWriter = new PrintWriter("writeFile.txt");
             printWriter.println("first line");
             try {
-                for (String line : titles) {
-                    titles.addAll(titles);
-                    Files.write(path, titles);
-                }
+                Files.write(path, withoutSmallthan3);
             } catch (ConcurrentModificationException e) {
-                e.printStackTrace();
+                e.getMessage();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -52,6 +61,17 @@ public class Main {
             System.out.println("Błąd zapisu do pliku.");
         }
 
+        File file = new File("/home/lukasz/Pulpit/javaCD/Workshop_1/src/pl/coderslab/Game_5/popularWords.txt");
+        StringBuilder reading = new StringBuilder();
+        try {
+            Scanner scan = new Scanner(file);
+            while (scan.hasNextLine()) {
+                reading.append(scan.nextLine() + "\n");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Brak pliku.");
+        }
+        System.out.println(reading.toString());
 
 
 
